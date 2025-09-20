5303e59ddf20de876e7808d04ae3a871699f5c1a
Loi-al <loaymrwan94@gmail.com>
2025-09-21 00:52:15 +0300

    back to clear branch

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "AzkarDataBase.db";
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 12;
     public SQLiteDatabase DB = null;
 
     // Table names and columns
