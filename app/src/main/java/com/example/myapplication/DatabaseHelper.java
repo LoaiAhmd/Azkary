dc20dfe15c4ad6dd3be5ec5f04b9146036c8cef0
Loi-al <loaymrwan94@gmail.com>
2025-09-21 17:09:17 +0300

    Finished IIII

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "AzkarDataBase.db";
-    public static final int DATABASE_VERSION = 16;
+    public static final int DATABASE_VERSION = 17;
 
     public static final String TBL_PRAYING_AZKAR = "tbl_praying_azkar";
     public static final String TBL_MORNING_AZKAR = "tbl_morning_azkar";
